6d3d022c224b5a3f20eba56c06abe6c9835ea389
erisenrholanda <58403137+erisenrholanda@users.noreply.github.com>
2022-11-29 16:27:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const navigation = document.querySelector('.navigation');
+const menuToggle = document.querySelector(".menu-toggle");
+const navigation = document.querySelector(".navigation");
 
 menuToggle.onclick = () => {
-    navigation.classList.toggle('open');
-}
+  navigation.classList.toggle("open");
+};
 
-const listItem = document.querySelectorAll('.list-item');
+const listItem = document.querySelectorAll(".list-item");
 
-listItem.forEach(item => {
-    item.onclick = () => {
-        listItem.forEach(item => item.classList.remove('active'));
+listItem.forEach((item) => {
+  item.onclick = () => {
+    listItem.forEach((item) => item.classList.remove("active"));
 
-        item.classList.add('active');
-    }
-})
+    item.classList.add("active");
+  };
+});
